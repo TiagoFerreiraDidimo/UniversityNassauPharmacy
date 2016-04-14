e63c5957c4653f2f8d3152ad225528d2cf7c58ac
Grupo(Tiago, Thiago) <Noah@Noah-PC>
2016-04-14 14:02:49 -0300

    Class Cliente extends Pessoa.

---
+++ Cliente.java
@@ -0,0 +1,15 @@
+package model;
+
+import java.util.LinkedList;
+
+public class Cliente extends Pessoa{
+	
+	//Constructor
+	public Cliente(String nome, int cpf, String endereco, String telefone) {
+		super(nome, cpf, endereco, telefone);
+		// TODO Auto-generated constructor stub
+	}
+	
+	//
+	
+}
